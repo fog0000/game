55c202264b4c081fdf561a848df1694384c886cb
fog0000 <wngp5366@gmail.com>
2024-11-19 01:41:29 +0900

    implement collide_blocks

--- implements.py
+++ implements.py
@@ -65,9 +65,9 @@ class Ball(Basic):
         pygame.draw.ellipse(surface, self.color, self.rect)
 
     def collide_block(self, blocks: list):
-        # ============================================
-        # TODO: Implement an event when the ball hits a block
-        pass
+        for block in blocks:
+            if block.alive and self.rect.colliderect(block.rect):
+                block.collide()
 
     def collide_paddle(self, paddle: Paddle) -> None:
         if self.rect.colliderect(paddle.rect):
