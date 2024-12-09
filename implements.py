1bde68e07b23501eba0d642fc6fff5310bb8feda
John Doe <hyeonchang88@gmail.com>
2024-12-09 22:16:03 +0900

    Review 일부 반영<#1>

--- implements.py
+++ implements.py
@@ -1,93 +1,3 @@
-<<<<<<< HEAD
-import math
-import random
-import time
-
-import config
-
-import pygame
-from pygame.locals import Rect, K_LEFT, K_RIGHT
-
-
-class Basic:
-    def __init__(self, color: tuple, speed: int = 0, pos: tuple = (0, 0), size: tuple = (0, 0)):
-        self.color = color
-        self.rect = Rect(pos[0], pos[1], size[0], size[1])
-        self.center = (self.rect.centerx, self.rect.centery)
-        self.speed = speed
-        self.start_time = time.time()
-        self.dir = 270
-
-    def move(self):
-        dx = math.cos(math.radians(self.dir)) * self.speed
-        dy = -math.sin(math.radians(self.dir)) * self.speed
-        self.rect.move_ip(dx, dy)
-        self.center = (self.rect.centerx, self.rect.centery)
-
-
-class Block(Basic):
-    def __init__(self, color: tuple, pos: tuple = (0,0), alive = True):
-        super().__init__(color, 0, pos, config.block_size)
-        self.pos = pos
-        self.alive = alive
-
-    def draw(self, surface) -> None:
-        pygame.draw.rect(surface, self.color, self.rect)
-    
-    def collide(self):
-        # ============================================
-        # TODO: Implement an event when block collides with a ball
-        pass
-
-
-class Paddle(Basic):
-    def __init__(self):
-        super().__init__(config.paddle_color, 0, config.paddle_pos, config.paddle_size)
-        self.start_pos = config.paddle_pos
-        self.speed = config.paddle_speed
-        self.cur_size = config.paddle_size
-
-    def draw(self, surface):
-        pygame.draw.rect(surface, self.color, self.rect)
-
-    def move_paddle(self, event: pygame.event.Event):
-        if event.key == K_LEFT and self.rect.left > 0:
-            self.rect.move_ip(-self.speed, 0)
-        elif event.key == K_RIGHT and self.rect.right < config.display_dimension[0]:
-            self.rect.move_ip(self.speed, 0)
-
-
-class Ball(Basic):
-    def __init__(self, pos: tuple = config.ball_pos):
-        super().__init__(config.ball_color, config.ball_speed, pos, config.ball_size)
-        self.power = 1
-        self.dir = 90 + random.randint(-45, 45)
-
-    def draw(self, surface):
-        pygame.draw.ellipse(surface, self.color, self.rect)
-
-    def collide_block(self, blocks: list):
-        # ============================================
-        # TODO: Implement an event when the ball hits a block
-        pass
-
-    def collide_paddle(self, paddle: Paddle) -> None:
-        if self.rect.colliderect(paddle.rect):
-            self.dir = 360 - self.dir + random.randint(-5, 5)
-
-    def hit_wall(self):
-        # ============================================
-        # TODO: Implement a service that bounces off when the ball hits the wall
-        pass
-        # 좌우 벽 충돌
-        
-        # 상단 벽 충돌
-    
-    def alive(self):
-        # ============================================
-        # TODO: Implement a service that returns whether the ball is alive or not
-        pass
-=======
 import math
 import random
 import time
@@ -138,7 +48,7 @@ class Block(Basic):
         self.alive = False  
     
     def drop_item(self, items: list):    # 추가: 아이템 생성 메서드
-        if random.random() <= 0.2:       # 20% 확률로 아이템 생성
+        if random.random() <= config.drop_probability:       # config.py에서 확률을 가져와 아이템 생성
             item_pos = (self.rect.centerx, self.rect.centery)
 
             # 랜덤으로 빨간 공 또는 파란 공 생성
@@ -198,4 +108,3 @@ class Ball(Basic):
          #여기도 마찬가지로 바로 config.display_dimension[1]로 가져다 쓰면 좋을 듯?
          screen_height = config.display_dimension[1]
          return self.rect.bottom < screen_height
->>>>>>> temp-fix
